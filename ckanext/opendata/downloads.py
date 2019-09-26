eca2ec17a263855a9dd6e59951e9c78712b9560f
dottyz <yizhao.tan@gmail.com>
2019-09-26 16:04:43 -0400

    Update struct of download functions

--- ckanext/opendata/downloads.py
+++ ckanext/opendata/downloads.py
@@ -18,14 +18,77 @@ import tempfile
 import constants
 
 
+def _write_datastore(params, resource):
+    format = params.get('format', constants.DOWNLOAD_FORMAT).lower()
+    projection = params.get('projection', constants.DOWNLOAD_PROJECTION)
+
+    is_geospatial = utils.is_geospatial(resource['id'])
+
+    assert (
+        (is_geospatial and format in constants.GEOSPATIAL_FORMATS) or \
+        (not is_geospatial and format in constants.TABULAR_FORMATS)
+    ), 'Inconsistency between data type and requested file format'
+
+    # Is this the best way to fetch data from datastore tables?
+    raw = requests.get('{host}/datastore/dump/{resource_id}'.format(
+        host=tk.config['ckan.site_url'],
+        resource_id=metadata['id']
+    )).content.decode('utf-8')
+
+    # WISHLIST: remove dependency on pandas/geopandas
+    df = pd.read_csv(io.StringIO(raw))
+
+    del raw
+
+    if is_geospatial:
+        df['geometry'] = df['geometry'].apply(
+            lambda x: shape(x)
+                if isinstance(x, dict)
+                else shape(json.loads(x))
+        )
+
+        df = gpd.GeoDataFrame(
+            df,
+            crs={
+                'init': 'epsg:{0}'.format(constants.DOWNLOAD_PROJECTION)
+            },
+            geometry='geometry'
+        ).to_crs({
+            'init': 'epsg:{0}'.format(projection)
+        })
+
+    # WISHLIST: store conversion in memory instead of write to disk
+    tmp_dir = tempfile.mkdtemp()
+    path = os.path.join(tmp_dir, '{0}.{1}'.format(metadata['name'], format))
+
+    output = iotrans.to_file(
+        df,
+        path,
+        projection=projection,
+        zip_content=(format=='shp')
+    )
+
+    del df
+
+    with open(output, 'rb') as f:
+        tk.response.write(f.read())
+
+    iotrans.utils.prune(tmp_dir)
+    gc.collect()
+
+    return '{fn}.{fmt}'.format(
+        fn=metadata['name'],
+        fmt=output.split('.')[-1]
+    )
+
 class DownloadsController(BaseController):
     def download_resource(self, resource_id):
-        metadata = tk.get_action('resource_show')(None, { 'id':resource_id })
+        resource = tk.get_action('resource_show')(None, { 'id':resource_id })
 
-        if not metadata['datastore_active']:
-            tk.redirect_to(metadata['url'])
+        if not resource['datastore_active']:
+            tk.redirect_to(resource['url'])
         else:
-            filename = self.get_datastore(metadata)
+            filename = _write_datastore(tk.request.GET, resource)
             mimetype, encoding = mimetypes.guess_type(filename)
 
             if mimetype is None and filename.endswith('gpkg'):
@@ -34,72 +97,3 @@ class DownloadsController(BaseController):
             tk.response.headers['Content-Type'] = mimetype
             tk.response.headers['Content-Disposition'] = \
                 (b'attachment; filename="{fn}"'.format(fn=filename))
-
-    def get_datastore(self, metadata):
-        format = tk.request.GET.get(
-            'format', constants.DOWNLOAD_FORMAT
-        ).lower()
-
-        projection = tk.request.GET.get(
-            'projection', constants.DOWNLOAD_PROJECTION
-        )
-
-        is_geospatial = utils.is_geospatial(metadata['id'])
-
-        if (is_geospatial and not format in constants.GEOSPATIAL_FORMATS) or \
-            (not is_geospatial and not format in constants.TABULAR_FORMATS):
-
-            raise tk.ValidationError({
-                'constraints': [
-                    'Inconsistency between data type and requested file format'
-                ]
-            })
-
-        raw = requests.get('{host}/datastore/dump/{resource_id}'.format(
-            host=tk.config['ckan.site_url'],
-            resource_id=metadata['id']
-        )).content.decode('utf-8')
-
-        df = pd.read_csv(io.StringIO(raw))
-
-        del raw
-
-        if is_geospatial:
-            df['geometry'] = df['geometry'].apply(
-                lambda x: shape(x)
-                    if isinstance(x, dict)
-                    else shape(json.loads(x))
-            )
-
-            df = gpd.GeoDataFrame(
-                df,
-                crs={
-                    'init': 'epsg:{0}'.format(constants.DOWNLOAD_PROJECTION)
-                },
-                geometry='geometry'
-            ).to_crs({
-                'init': 'epsg:{0}'.format(projection)
-            })
-
-        tmp_dir = tempfile.mkdtemp()
-        path = os.path.join(tmp_dir, '{0}.{1}'.format(metadata['name'], format))
-
-        output = iotrans.to_file(
-            df,
-            path,
-            projection=projection,
-            zip_content=(format=='shp')
-        )
-
-        del df
-
-        with open(output, 'rb') as f:
-            tk.response.write(f.read())
-
-        iotrans.utils.prune(tmp_dir)
-        gc.collect()
-
-        return '{fn}.{fmt}'.format(
-            fn=metadata['name'],
-            fmt=output.split('.')[-1]
-        )
