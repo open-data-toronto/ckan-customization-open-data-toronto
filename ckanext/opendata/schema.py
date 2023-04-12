7d8b977a8952243504870167d40b737070997462
mnichol4 <mackenzie.nichols4@toronto.ca>
2023-04-12 18:56:38 +0000

    PEP8 and code cleaning

--- ckanext/opendata/schema.py
+++ ckanext/opendata/schema.py
@@ -1,9 +1,5 @@
-from six import string_types
-
-from . import utils
-
 import ckan.plugins.toolkit as tk
-import logging
+
 
 def create_resource_views(context, resource):
     # creates the views for resources to be viewed in the CKAN UI
@@ -32,19 +28,31 @@ def create_resource_views(context, resource):
     views = tk.get_action("resource_view_list")(context, {"id": resource["id"]})
     existing_view_types = []
     for view in views:
-        existing_view_types.append( view["view_type"] )
-        #tk.get_action("resource_view_delete")(context, {"id": view["id"] }) 
+        existing_view_types.append(view["view_type"])
+        # tk.get_action("resource_view_delete")(context, {"id": view["id"] })
 
     # make a data explorer view for all resources
     if "recline_view" not in existing_view_types:
         tk.get_action("resource_view_create")(context, format_views["data explorer"])
 
     # make a map view for a resource with a 'geometry' field
-    if "geometry" in [field["id"] for field in tk.get_action("datastore_search")(context, {"id": resource["id"], "limit":0})["fields"]] and "recline_map_view" not in existing_view_types:
+    if (
+        "geometry"
+        in [
+            field["id"]
+            for field in tk.get_action("datastore_search")(
+                context, {"id": resource["id"], "limit": 0}
+            )["fields"]
+        ]
+        and "recline_map_view" not in existing_view_types
+    ):
         tk.get_action("resource_view_create")(context, format_views["map"])
 
+
 def update_package(context):
-    # Ensures that changes to a resource in a package also affect the package's metadata
+    '''Ensures that changes to a resource in a package also affect
+    the package's metadata'''
+
     package = context["package"]
     resources = [r for r in package.resources_all if r.state == "active"]
 
@@ -53,32 +61,41 @@ def update_package(context):
 
     for r in resources:
         resource_format = r.format.upper()
-        
+
         # add all resource formats to the package's list of formats
         formats.add(resource_format)
 
-        # add possible last refreshed dates to array, to be sorted through below
+        # add possible last refreshed dates to array, sorted through below
         last_refreshed.append(r.created if r.last_modified is None else r.last_modified)
-
 
     formats = ",".join(list(formats)) if len(formats) else None
 
-    # make sure the package's last refreshed date is the latest last refreshed date of its resources
+    # make sure the package's last refreshed date is the latest last
+    # refreshed date of its resources
     last_refreshed = (
         max(last_refreshed).strftime("%Y-%m-%dT%H:%M:%S.%f")
         if len(last_refreshed)
         else None
     )
 
-    # If the last refreshed date isnt what it already is in the CKAN package, update the package
-    if "last_refreshed" in tk.get_action("package_show")(context, {"id": package.id}).keys():
-        old_last_refreshed = tk.get_action("package_show")(context, {"id": package.id})["last_refreshed"]
+    # If the last refreshed date isnt what it already is in the CKAN package,
+    # update the package
+    if (
+        "last_refreshed"
+        in tk.get_action("package_show")(context, {"id": package.id}).keys()
+    ):
+        old_last_refreshed = tk.get_action("package_show")(context, {"id": package.id})[
+            "last_refreshed"
+        ]
     else:
         old_last_refreshed = None
-        
+
     if last_refreshed != old_last_refreshed:
-        package_patch = tk.get_action("package_patch")(
+        tk.get_action("package_patch")(
             context,
-            {"id": package.id, "last_refreshed": last_refreshed, "formats": formats},
+            {
+                "id": package.id,
+                "last_refreshed": last_refreshed,
+                "formats": formats
+            },
         )
-  
--- ckanext/opendata/utils.py
+++ ckanext/opendata/utils.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 import ckan.plugins.toolkit as tk
-import mimetypes
 import json
 import csv
 import re
@@ -22,39 +21,27 @@ def is_geospatial(resource_id):
     return "geometry" in info["schema"]
 
 
-def to_list(l):
-    if not isinstance(l, list):
-        return [l]
+def to_list(input):
+    if not isinstance(input, list):
+        return [input]
     else:
-        # If the item is already a list from wordpress, it 
-        # may have "vocab_" as an unnecessary prefix to certain 
+        # If the item is already a list from wordpress, it
+        # may have "vocab_" as an unnecessary prefix to certain
         # values, specifically when running extendedapi's /search_facets
-        return [item.replace("vocab_", "") if item.startswith("vocab_") else item for item in l]
+        return [
+            item.replace("vocab_", "") if item.startswith("vocab_") else item
+            for item in input
+        ]
 
 
 def validate_length(key, data, errors, context):
     max_length = 350
     if data[key] and len(data[key]) > max_length:
         raise tk.ValidationError(
-            {
-                "constraints": [
-                    "Input exceed {0} character limit".format(
-                        max_length
-                    )
-                ]
-            }
+            {"constraints": ["Input exceed {0} character limit".format(max_length)]}
         )
 
     return data[key]
-
-
-def validate_tag_in_vocab(tag, vocab):
-    try:
-        tk.get_action("tag_show")(None, {"id": tag, "vocabulary_id": vocab})
-    except:
-        raise tk.ValidationError(
-            {"constraints": ["Tag {0} is not in the vocabulary {1}".format(tag, vocab)]}
-        )
 
 
 def unstringify(input):
@@ -66,49 +53,61 @@ def unstringify(input):
     terms = []
     output = []
 
-    assert isinstance(input, list), "Input to unstringify is not a list, its {}".format(type(input))
-    
+    assert isinstance(input, list), "Input to unstringify is not a list, its {}".format(
+        type(input)
+    )
+
     # for each dict in the input...
     for item in input:
-        
-        assert isinstance(item, dict), "Input list to unstringify does not contain dicts"
+        assert isinstance(
+            item, dict
+        ), "Input list to unstringify does not contain dicts"
         assert "name" in item.keys(), "Input list's dict doesnt have a name attribute"
         assert "count" in item.keys(), "Input list's dict doesnt have a count attribute"
 
         # take the item out of its list, and put them in one big array
-        
+
         if isinstance(item["name"], str):
-            these_names = item["name"].replace("{", "").replace("}", "").replace('"', "").split(",") 
+            these_names = (
+                item["name"]
+                .replace("{", "")
+                .replace("}", "")
+                .replace('"', "")
+                .split(",")
+            )
             names += these_names
-            terms.append( {"names": these_names, "count": item["count"]} )
+            terms.append({"names": these_names, "count": item["count"]})
 
     # get the distinct terms and make an output dict structure for them
     for name in set(names):
-        item = {
-            "count": 0,
-            "display_name": name,
-            "name": name
-        }
+        item = {"count": 0, "display_name": name, "name": name}
         # update the count attribute in the appropriate dict in the output dict
         for value in terms:
             if name in value["names"]:
                 item["count"] += value["count"]
-        
-        output.append( item )
-    
+
+        output.append(item)
+
     return output
 
 
 # Useful scheming validator functions
 # ===
 def choices_to_string(value):
-
     if isinstance(value, list):
         return ", ".join(value)
     elif isinstance(value, dict):
         return json.dumps(value)
     elif isinstance(value, str):
-        return value.replace('\\', '').replace("[", "").replace("]", "").replace('\"', '').replace("{", "").replace("}", "")
+        return (
+            value.replace("\\", "")
+            .replace("[", "")
+            .replace("]", "")
+            .replace('"', "")
+            .replace("{", "")
+            .replace("}", "")
+        )
+
 
 def string_to_choices(value):
     if isinstance(value, str):
@@ -116,72 +115,89 @@ def string_to_choices(value):
     else:
         return value
 
+
 def default_to_none(value):
     if value:
         return value
 
+
 def default_to_false(value):
-    if isinstance(value, string_types):
+    if isinstance(value, str):
         return value.lower() == "true"
 
     return bool(value)
 
+
 def default_to_today(value):
-    # if we receive a valid datetime IS format string, parse it into an ISO format datetime object
+    # if we receive a valid datetime IS format string, parse it into 
+    # an ISO format datetime object
     # if we receive a datetime, return it as is
     # if we return something else, return today as a datetime object
-    
+
     if isinstance(value, str):
         return str_to_datetime(value)
-        
+
     elif isinstance(value, datetime):
         return value
     else:
         return datetime.today()
 
+
 def datastore_to_csv(resource_id, data, filepath):
-    # In ckan <2.9.3, the lazyjson object only works as a normal dict when you take its 0th index
-    
-    with open("/usr/lib/ckan/default/src/ckanext-opendatatoronto/ckanext/opendata/" + resource_id + ".csv", "w") as file:
+    # In ckan <2.9.3, the lazyjson object only works as a normal dict
+    # when you take its 0th index
+
+    with open(
+        "/usr/lib/ckan/default/src/ckanext-opendatatoronto/ckanext/opendata/"
+        + resource_id
+        + ".csv",
+        "w",
+    ) as file:
         writer = csv.writer(file)
         headers = data[0].keys()
-        writer.writerow( headers )
+        writer.writerow(headers)
         for row in data:
             assert row.keys() == headers
             writer.writerow(row.values())
     file.close()
-    
+
+
 def lazyjson_to_dict(lazyjson):
     output = []
     for item in lazyjson:
-        output.append( item )
+        output.append(item)
     return output
 
+
 def str_to_datetime(input):
-    # loops through the list of formats and tries to return an input string into a datetime of one of those formats
-    assert isinstance(input, str), "Utils str_to_datetime() function can only receive strings - it instead received {}".format(type(input))
+    # loops through the list of formats and tries to return an input
+    # string into a datetime of one of those formats
+    assert isinstance(
+        input, str
+    ), "str_to_datetime() input should be string - it received {}".format(
+        type(input)
+    )
     for format in [
         "%Y-%m-%dT%H:%M:%S.%f",
         "%Y-%m-%d %H:%M:%S.%f",
         "%Y-%m-%dT%H:%M:%S",
         "%Y-%m-%d %H:%M:%S",
-        "%Y-%m-%d"
+        "%Y-%m-%d",
     ]:
         try:
             output = datetime.strptime(input, format)
             return output
         except ValueError:
             pass
-    print("No valid datetime format in utils.str_to_datetime() for input string {}".format(input))
+    print(
+        "No valid datetime format in utils.str_to_datetime() for input string {}".format(
+            input
+        )
+    )
 
-def default_to_false(value):
-    if value in [True, "true", "True", "TRUE"]:
-        return True
-    else:
-        return False
 
 def list_to_words(input):
-    if isinstance(input,str):
+    if isinstance(input, str):
         return input.split(" ")
     elif isinstance(input, list):
         output = []
@@ -192,21 +208,30 @@ def list_to_words(input):
 
 
 def get_catalog():
-    '''gets catalog datastore resource as json object'''
+    """gets catalog datastore resource as json object"""
 
     try:
         package = tk.get_action("package_show")(data_dict={"id": "od-etl-configs"})
         resource_id = package["resources"][0]["id"]
-        output = tk.get_action("datastore_search")(data_dict={"resource_id": resource_id, "limit": 32000}) 
+        output = tk.get_action("datastore_search")(
+            data_dict={"resource_id": resource_id, "limit": 32000}
+        )
         output["url"] = package["resources"][0]["url"]
     except Exception as e:
         print("Couldnt access catalog page:\n" + str(e))
-        output = {"records": [{"message": "Log in as an administrator to see the catalog's ETL details on this page"}]}
+        output = {
+            "records": [
+                {
+                    "message": "Log in as an admin to see catalog ETL details"
+                }
+            ]
+        }
 
     return output
 
+
 def to_boolean(input):
-    '''Input string representation of boolean, output boolean'''
+    """Input string representation of boolean, output boolean"""
     if isinstance(input, bool):
         return input
     if isinstance(input, int):
@@ -218,8 +243,9 @@ def to_boolean(input):
     else:
         return input
 
+
 def clean_civic_issues(input):
-    '''Translate input string to civic issue or return error'''
+    """Translate input string to civic issue or return error"""
     output = []
     package = tk.get_action("package_show")(None, {"id": "metadata-catalog"})
 
@@ -233,9 +259,8 @@ def clean_civic_issues(input):
     # standardize strings and compare. If match, return correct civic issue
     for this_input in input:
         for issue in correct_civic_issues:
-
-            working_input = re.sub(r'[^a-zA-Z0-9]', '', this_input).lower()
-            working_issue = re.sub(r'[^a-zA-Z0-9]', '', issue).lower()
+            working_input = re.sub(r"[^a-zA-Z0-9]", "", this_input).lower()
+            working_issue = re.sub(r"[^a-zA-Z0-9]", "", issue).lower()
             if working_issue == working_input:
                 output.append(issue)
 
@@ -243,9 +268,7 @@ def clean_civic_issues(input):
         raise tk.ValidationError(
             {
                 "constraints": [
-                    "Input {} didnt match any civic issues".format(
-                        str(input)
-                    )
+                    "Input {} didnt match any civic issues".format(str(input))
                 ]
             }
         )
@@ -254,7 +277,7 @@ def clean_civic_issues(input):
 
 
 def clean_topics(input):
-    '''Translate input string to topic or return error'''
+    """Translate input string to topic or return error"""
     output = []
     package = tk.get_action("package_show")(None, {"id": "metadata-catalog"})
 
@@ -268,21 +291,14 @@ def clean_topics(input):
     # standardize strings and compare. If match, return correct civic issue
     for this_input in input:
         for issue in correct_topics:
-
-            working_input = re.sub(r'[^a-zA-Z0-9]', '', this_input).lower()
-            working_issue = re.sub(r'[^a-zA-Z0-9]', '', issue).lower()
+            working_input = re.sub(r"[^a-zA-Z0-9]", "", this_input).lower()
+            working_issue = re.sub(r"[^a-zA-Z0-9]", "", issue).lower()
             if working_issue == working_input:
                 output.append(issue)
 
     if len(output) == 0 and len(input) > 0:
         raise tk.ValidationError(
-            {
-                "constraints": [
-                    "Input {} didnt match any topics".format(
-                        str(input)
-                    )
-                ]
-            }
+            {"constraints": ["Input {} didnt match any topics".format(str(input))]}
         )
 
     return output
